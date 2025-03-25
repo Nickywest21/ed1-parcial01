dec72eabf0e411c82fe0e280f8243b7a8985d7c9
Nickywest21 <158302385+Nickywest21@users.noreply.github.com>
2025-03-25 07:52:25 -0600

    Parcial #1 - Nicolle West - 1245524

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -1,9 +1,30 @@
 package ed.lab;
-
+//Código realiza por Nicolle West - 1245524
 public class E01FlattenBT {
 
     public void flatten(TreeNode<Integer> root) {
+        if (root == null)
+        {
+            return;
+        }
 
+        flatten(root.left);
+        flatten(root.right);
+
+        TreeNode<Integer> left = root.left;
+        TreeNode<Integer> right = root.right;
+
+        root.left = null;
+        root.right = left;
+
+        TreeNode<Integer> current = root;
+        while (current.right !=null)
+        {
+            current = current.right;
+        }
+        current.right = right;
     }
 
 }
+
+
